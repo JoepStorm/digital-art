f9146cf05c0f8395debffdb9d255df5079254b5e
Joep <j.storm@tudelft.nl>
2026-02-11 21:37:46 +0100

    Update to iteration 6 snapshots

---
+++ snapshots/manifest.js
@@ -0,0 +1,29 @@
+window.MANIFEST = {
+  "latest": 5,
+  "iterations": [
+    {
+      "num": 0,
+      "description": "Original"
+    },
+    {
+      "num": 1,
+      "description": "Slightly increase agent color opacity"
+    },
+    {
+      "num": 2,
+      "description": "Add a slight random wobble to the agent direction"
+    },
+    {
+      "num": 3,
+      "description": "Add a subtle trail decay effect"
+    },
+    {
+      "num": 4,
+      "description": "Increase the number of update iterations to make the trails more dense"
+    },
+    {
+      "num": 5,
+      "description": "Add a slight color variation to the trail based on the agent's direction"
+    }
+  ]
+};
